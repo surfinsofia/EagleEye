5989ded53ec3fcd2282907dc66832d91d9bb9839
thechicketarian <thechicketarian@gmail.com>
2022-04-19 18:47:38 -0500

    File Structure

--- fetch_practice/script.js
+++ fetch_practice/script.js
@@ -6,7 +6,7 @@ fetchButton.addEventListener("click",getApi )
 function getApi() {
   
   // Insert the API url to get a list of your repos
-  var requestUrl = 'https://api.spotify.com/v1/artists/1vCWHaC5f2uS3yhpwWbIA6/albums?album_type=SINGLE&offset=20&limit=10';
+  var requestUrl = '';
 
   fetch(requestUrl)
     .then(function (response) {
