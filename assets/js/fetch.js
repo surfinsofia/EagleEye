a7b84741fdcf69b25d9fb6c2d05c92df3036bde9
jesusoyer <oyervidesjesus017@gmail.com>
2022-04-27 22:02:09 -0500

    new code for data structure

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -1,3 +1,13 @@
+var today = moment().format("L");
+// Dark and Light mode function
+function darkLightMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+$("#dark-mode").click(darkLightMode);
+
+
 var parkName = document.querySelector("#sPark")
 var cityName = document.querySelector("#apiCityBox")
 var stateName = document.querySelector("#apiStateBox")
@@ -39,7 +49,7 @@ function getApi() {
       
       newArray=[]
       
-     console.log(newArray)
+     
       for(let i=0; i<=10; i++){       
         var parkInfo = {
           name:  data.data.fullName,
@@ -51,106 +61,78 @@ function getApi() {
           
       };
       console.log(data.data[i].addresses[1].city)
-         newArray.push(parkInfo)
-        
-        window.localStorage.setItem("parkInfo",JSON.stringify(newArray))
-          
-        
-      }
+      // pushing the parkinfo object into an array named newArray  
+      newArray.push(parkInfo)
+        //storing newArray into local storage
+        window.localStorage.setItem("parkInfo",JSON.stringify(newArray))              }
      
-      //  console.log(localStorage.parkInfo)
-    
-        currentCityWeather();
+         storeCitiesInLocalStorage();
     });
 }
-// var test = {conditions:["hot",'cold','misty','windy','gusty']}
-  
 
 
-// var storedNames = JSON.parse(localStorage.getItem("parkInfo"));
-// console.log(storedNames[0].weather)
-// console.log(storedNames)
-
-
-
-// for(let i=0;i<storedNames.length;i++){
-//   console.log(storedNames[i].city)
-//   storedNames[i].weather= test.conditions[i]
-  
-
+function storeCitiesInLocalStorage(){
+  // getting parkInfo from local storage and storing it in a variable named storedNames
 var storedNames = JSON.parse(localStorage.getItem("parkInfo"));
 console.log(storedNames)
+var citiesArray =[]
 
+
+console.log(citiesArray)
 for(let i=0;i<storedNames.length;i++){
-  console.log(storedNames[i])
-
-// //loop to append elements to page
-// for(let i=0;i<storedNames.length;i++){
-//   console.log(storedNames[i].weather)
-  
-  
-  
-//   const para = document.createElement("p");
-//   para.innerHTML =storedNames[i].weather ;
-//   document.getElementById("sPark").appendChild(para);
-// }
-
-
-
-// var newKey=storedNames[0].weather="hot"
-
-
-
-
-
-
-
-
-
-
-// Dark and Light mode function
-function darkLightMode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
- }
-
- $("#dark-mode").click(darkLightMode);
-
-// weather for each City
-
-
-// cities array 
-//  var city =["Austin","Dallas", "Houston", "San Antonio", "amarillo"];
- var today = moment().format("L");
- 
-
+  //looping through storedNames position and pushing the cities into a new citiesArray
+  citiesArray.push(storedNames[i].city)
+  //storing cities array into local storage 
+  window.localStorage.setItem("citiesFromData",JSON.stringify(citiesArray))
+  currentCityWeather()
+}}
  
 
  function currentCityWeather (){
 
-  var cityFromLocalStorage = JSON.parse(localStorage.getItem("cityFetch"))
-console.log(cityFromLocalStorage)
-   for (var i = 0 ; i < cityFromLocalStorage.length; i++){
+  var cityFromLocalStorage = JSON.parse(localStorage.getItem("citiesFromData"))
+  var weatherArray =[]
+  
+
+for (var i = 0 ; i < cityFromLocalStorage.length; i++){
        // weather api
-       var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityFromLocalStorage[i] + "&units=imperial&appid=1ca11eb2eb0a3f14218b80de67213ac5"
+       var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityFromLocalStorage[i] + "&units=imperial&appid=ec09eb5352a3fea1cbdc287501e99a39"
          fetch(requestUrl)
         .then(function (response) {
             return response.json();
          })
       .then(function (weatherResponse) {
-         console.log(weatherResponse);
-         console.log(weatherResponse.main.temp);
+         console.log(weatherResponse); 
+        // pushing weather response data into an array named weatherArray
+
+
+         weatherArray.push(weatherResponse)
+        
+        
+        
+        
+         //  var weatherToparkInfo = JSON.parse(localStorage.getItem("parkInfo"));
+        //  console.log(weatherToparkInfo)
+
+       
+
+
       // append the weather result on the browser
-           var currentcity = $(`
-              <h4 id="currentState">
-                  ${weatherResponse.name} ${today}
-              </h4>
-              <p>Temperature: ${weatherResponse.main.temp} °F</p>
-              <p>Humidity: ${weatherResponse.main.humidity}\%</p>
-         `   );
-          $("#apiWeatherBox1").append(currentcity);
+      //      var currentcity = $(`
+      //         <h4 id="currentState">
+      //             ${weatherResponse.name} ${today}
+      //         </h4>
+      //         <p>Temperature: ${weatherResponse.main.temp} °F</p>
+      //         <p>Humidity: ${weatherResponse.main.humidity}\%</p>
+      //    `   );
+      //     $("#apiWeatherBox1").append(currentcity);
+
+      //storing weatherArray into local storage
+      window.localStorage.setItem("weatherForCites",JSON.stringify(weatherArray))
+
        });
      }
+     getAlertsApi();
  }
  
  
@@ -160,7 +142,7 @@ console.log(cityFromLocalStorage)
 function getAlertsApi() {
   var stateStorage = window.localStorage.getItem("state")
 
-  var requestAlertsUrl = 'https://developer.nps.gov/api/v1/alerts?parkCode=&stateCode='+stateStorage+'&api_key=RoK1dEqNYGy3sRg4nQLmqFuSBnd6vuo9p4WA8l9Q';
+  var requestAlertsUrl = 'https://developer.nps.gov/api/v1/alerts?parkCode=&stateCode='+stateStorage+'&api_key=ec09eb5352a3fea1cbdc287501e99a39';
 
   fetch(requestAlertsUrl)
     .then(function (response) {
@@ -188,47 +170,39 @@ function getAlertsApi() {
       // console.log(data.data[0].description);
       // console.log(data.data[0].lastIndexedDate);
       // webTest.textContent="";
-      // console.log(webTest);   
+      // console.log(webTest);  
+    
     }
     });
-  
+    
 }
 //test to see if it works
-getAlertsApi()
+function dataStructureCreated(){
+  //accessing parkInfo in local storage
+var newData = JSON.parse(localStorage.getItem("parkInfo"))
+console.log(newData)
+//accessing weatherForCities in local storage
+var newWeatherData = JSON.parse(localStorage.getItem("weatherForCites"))
+console.log(newWeatherData)
+// accessing parkAlertsInfo in local storage
+var parkAlerts = JSON.parse(localStorage.getItem("parkAlertsInfo"))
+console.log(parkAlerts)
 
 
+for(let i=0; i<newData.length; i++){
+// storing adding weather and alert data to newData object
+newData[i].temp = newWeatherData[i].main.temp
+newData[i].windspeed= newWeatherData[i].wind.speed
+newData[i].humidity= newWeatherData[i].main.humidity
+newData[i].description = newWeatherData[i].weather[0].description
+newData[i].icon = newWeatherData[i].weather[0].icon
+newData[i].alert = parkAlerts[i].title
+newData[i].alertType = parkAlerts[i].description
+newData[i].alertDate = parkAlerts[i].date
 
-
-
-    //     $("#apiWeatherBox3").append(currentState);
-    // });
-    // ;
-   
-
-
-         
-          //  console.log(localStorage.parkInfo)
-        
-    
-     
-
-
-    
-  
-
-
-
-
-
-    
- 
-
-
-
-
-
-
-
-
-
-
+console.log(newData)
+//saving final data in local storage
+window.localStorage.setItem("finalData",JSON.stringify(newData))
+}
+}
+dataStructureCreated();
